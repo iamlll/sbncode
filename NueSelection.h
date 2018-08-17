df0c0434d89be0a2ce0bb56a41d1a40c5717479c
Lisa Lin <llin@sbndbuild01.fnal.gov>
2018-08-17 17:52:47 -0500

    WHY DO YOU DO THIS TO ME MCSHOWER

---
+++ NueSelection.h
@@ -0,0 +1,78 @@
+#ifndef __sbnanalysis_ana_SBNOsc_NueSelection__
+#define __sbnanalysis_ana_SBNOsc_NueSelection__
+
+/**
+ * \file NueSelection.h
+ *
+ * SBN nue selection.
+ *
+ * Author: 
+ */
+
+#include <iostream>
+#include "canvas/Utilities/InputTag.h"
+#include "core/SelectionBase.hh"
+#include "core/Event.hh"
+#include "lardataobj/MCBase/MCShower.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "lardataobj/MCBase/MCTrack.h"
+
+//Forward declarations
+class TH2D;
+
+namespace ana {
+  namespace SBNOsc {
+
+/**
+ * \class NueSelection
+ * \brief Electron neutrino event selection
+ */
+class NueSelection : public core::SelectionBase {
+public:
+  /** Constructor. */
+  NueSelection();
+
+  /**
+   * Initialization.
+   *
+   * \param config A configuration, as a JSON object
+   */
+  void Initialize(Json::Value* config=NULL);
+
+  /** Finalize and write objects to the output file. */
+  void Finalize();
+
+  /**
+   * Process one event.
+   *
+   * \param ev A single event, as a gallery::Event
+   * \param Reconstructed interactions
+   * \return True to keep event
+   */
+  bool ProcessEvent(const gallery::Event& ev, std::vector<Event::Interaction>& reco);
+
+protected:
+  unsigned fEventCounter;  //!< Count processed events
+  unsigned fNuCount;  //!< Count num nu_e CCQE events
+
+  /** Configuration parameters */
+  art::InputTag fTruthTag;  //!< art tag for MCTruth information
+};
+  art::InputTag fTrackTag; //MCTrack
+  art::InputTag fShowerTag;//MCShower
+
+  /**histograms!*/
+  TH2D* fNuVertexXZHist;  //!< Neutrino vertex XZ projection
+  TH2D* fNuVertexXYHist; //nu vertex XY proj
+  TH2D* fNuVertexYZHist; //nu vertex YZ proj
+  TH2D* fTrackXY;
+  TH2D* fTrackYZ;
+  TH2D* fTrackXZ;
+  TH2D* fShowerXY;
+  TH2D* fShowerYZ;
+  TH2D* fShowerXZ;
+  }  // namespace SBNOsc
+}  // namespace ana
+
+#endif  // __sbnanalysis_ana_SBNOsc_NueSelection__
+
